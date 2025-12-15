bd0ab82d36c14dee7123fb394ed4bd4736ad3ddd
IamChandu114 <ca4443700@gmail.com>
2025-12-15 15:32:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from explanation_engine import generate_explanation
 
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='index.html', static_folder='static')
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -84,6 +84,7 @@ def index():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
